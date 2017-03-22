0396bc711e954884d3df80859f1af9922947ae61
cf0702 <caofeng0702@163.com>
2017-03-22 16:22:07 +0800

    加入显示天气的功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,19 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by 曹峰 on 2017/3/22.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
